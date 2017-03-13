3ab81cc9589e3eded2b8b804ac68f19959cb9b40
Elmer <elmermelendez8210@gmail.com>
2017-03-13 05:07:23 -0400

    made v2 of script

--- code2PDF.sh
+++ code2PDF.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
+#script still needs commenting on how it works
+
+#TODO add invalid syntax if user does not give -o flag
+#TODO add check to make sure there is given named file
+
+
 clear
 
 echo "Created by Elmer Melendez"
 echo -e "Script starting....\n"
-echo "Please type the file you'd like to make into a PDF: "
-read fileName
+
+if [ $# -eq 0 ]
+then
+ echo No arguments provided
+ echo Example: $ ./code2PDF file1 file2 file3 -o MergedName
+ exit 1
+fi
+
+args=( "$@" )
 
 
-enscript -C --margins=50:50:50:50 $fileName -o $fileName.ps
-ps2pdf $fileName.ps
-rm $fileName.ps
-fileName=$fileName.pdf
-newName=${fileName%%.*}.pdf
-mv $fileName $newName
+for fileName in "$@"
+do
+if [ "$fileName" = "-o" ]; then
+	n=$#
+	offset=$((n-2))
+	outputName=${!n}
+	pass=${args[*]:0:$offset}
+	pdftk ${pass//.c} output "${outputName}.pdf"
+	rm ${pass//.c}
+
+	exit 0
+else
+	enscript -C --margins=50:50:50:50 "$fileName" -o "$fileName.ps"
+	ps2pdf "$fileName.ps"
+	rm "$fileName.ps"
+	fileName=$fileName.pdf
+	defaultName="${fileName%%.*}"
+	mv "$fileName" "$defaultName"
+fi
+done
+
+
+
+
+
